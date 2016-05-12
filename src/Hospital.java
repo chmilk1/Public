9f154d648d2852e6b99d2832f11c947c2973a986
S <league@iMac.attlocal.net>
2016-05-11 18:27:57 -0700

    TODO: add funds
    serous todos? dude stop

---
+++ src/Hospital.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hospital {
+	//TODO remove TODOs
+	List<Doctor> Doctors = new ArrayList<Doctor>();
+	List<Patient> consumers = new ArrayList<Patient>();
+	public List<Doctor> getDoctors() {
+		return Doctors;
+	}
+
+	public void addDoctor(Doctor j) {
+	Doctors.add(j);	
+	}
+
+	public void addPatient(Patient patient) {
+		consumers.add(patient);
+	}
+
+	public List<Patient> getPatients() {
+		return consumers;
+	}
+
+	public void addDoctor(GeneralPractitioner j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+}
